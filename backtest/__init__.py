6b7ba702f05c930b6d31b4b256d7620b142ab269
felippesenne <86943746+felippesenne@users.noreply.github.com>
2026-09-13 02:25:41 -0300

    feat(backtest): add backtest package

---
+++ backtest/__init__.py
@@ -0,0 +1,4 @@
+from .engine import BacktestConfig, BacktestResult, run_backtest
+from .metrics import compute_metrics
+
+__all__ = ["BacktestConfig", "BacktestResult", "run_backtest", "compute_metrics"]
